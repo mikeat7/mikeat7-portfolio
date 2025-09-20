9141de85c471e75308c711adba588da857f5a41d
mikeat7 <ekimat7@rogers.com>
2025-09-20 15:37:10 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import { VXProvider } from '@/context/VXProvider';
 import './index.css';
 
